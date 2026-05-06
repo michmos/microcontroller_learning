cf15d54d0b54125e705b3d00fcac020d94c8cde2
Michael Moser <Michael.Moser11@web.de>
2026-05-06 12:37:57 +0200

    refactor: rmv magic numbers

--- PWM/16_brightnessControl/16_brightnessControl.ino
+++ PWM/16_brightnessControl/16_brightnessControl.ino
@@ -1,6 +1,8 @@
 #define LED_PIN 5
 #define PWM_RESOLUTION 255
-#define PWM_FREQUENCY 980
+#define PWM_FREQUENCY 980  // in Hz
+#define INCREASE_TIME 2    // in seconds
+#define DECREASE_TIME 1    // in seconds
 
 const float CYCLE_TIME = 1.0 / PWM_FREQUENCY;
 
@@ -14,7 +16,7 @@ void loop() {
   unsigned long startTime = millis();
   for (int dutyCycle = 0; dutyCycle <= PWM_RESOLUTION; ++dutyCycle) {
     analogWrite(LED_PIN, dutyCycle);
-    delayMicroseconds(2000000 / 255);
+    delayMicroseconds(INCREASE_TIME * 1000000 / PWM_RESOLUTION);
   }
   Serial.print("Increase: ");
   Serial.println(millis() - startTime);
@@ -23,7 +25,7 @@ void loop() {
   // decrease brighntess
   for (int dutyCycle = PWM_RESOLUTION; dutyCycle >= 0; --dutyCycle) {
     analogWrite(LED_PIN, dutyCycle);
-    delayMicroseconds(1000000 / 255);
+    delayMicroseconds(DECREASE_TIME * 1000000 / PWM_RESOLUTION);
   }
   Serial.print("Decrease: ");
   Serial.println(millis() - startTime);

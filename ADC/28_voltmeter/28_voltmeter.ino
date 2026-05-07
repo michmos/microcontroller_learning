b788ca3f5a986eeb044060e39ae449adfad4894a
Michael Moser <Michael.Moser11@web.de>
2026-05-07 11:34:15 +0200

    add 28_voltmeter

---
+++ ADC/28_voltmeter/28_voltmeter.ino
@@ -0,0 +1,30 @@
+#define ADC_PIN A5
+#define ADC_RESOLUTION 1024
+
+uint8_t mode = DEFAULT;
+float vRef = 5;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  uint16_t potVal = analogRead(ADC_PIN);
+  float volts = (float)potVal / (ADC_RESOLUTION - 1) * vRef;
+  Serial.print("val: ");
+  Serial.print(potVal);
+  Serial.print(" volts: ");
+  Serial.println(volts);
+
+  // updating the reference voltage to be more precise for smaller voltages
+  if (volts < 1.1 && vRef != 1.1) {
+    Serial.println("Updating analog reference to internal");
+    analogReference(INTERNAL);
+    vRef = 1.1;
+
+  } else if (volts >= 1.1 && vRef != 5) {
+    Serial.println("Updating analog reference to default");
+    analogReference(DEFAULT);
+    vRef = 5;
+  }
+}
